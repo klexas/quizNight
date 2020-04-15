4c9ac99380584c90f243a267714e27cfb0d4e499
Adam Robins <iam@adamrobins.com>
2020-04-15 02:26:21 +0100

    WIP : dataservice layer should have come first

--- client/app/services/room/roomService.js
+++ client/app/services/room/roomService.js
@@ -1,8 +1,16 @@
-define(['plugins/http', 'durandal/app', 'knockout', 'socket'], function (http, app, ko, io) {
+define(['services/data'], function (dataService) {
     return {
-        checkRoom: (room_name)=>{
-            // TODO: sbagliato - API
-            return (room_name === 'test');
+        checkRoom: async (room_name) => {
+            // TODO: Lol - sanitize here
+            var roomAvailable = await dataService.room.exists(room_name);
+            if(roomAvailable == 404)
+            // JS allows on the fly typing(if you can even call it that).
+            // Sure why not embrace it. Hedonism at it's finest.
+                return (true);
+            else return false; // Who cares at this stage, the room cant be created.
+        },
+        addRoom: (room_model) => {
+            console.log('lol some more ? If this works it will be cool');
         }
     }
 })
--- common/errors.js
+++ common/errors.js
@@ -12,5 +12,7 @@ module.exports = {
         return messages.http[Object.keys(messages.http[0]).filter((code) => {
             return messages.http[code][0] == response;
         })];
+        // Because we all love how perl and php was magic too
+        // Pure debauchery. Love it.
     }
 }
